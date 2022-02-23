579570ebac3332374fc3f11b985ffb81ef2c0e92
gdelataillade <gautier2406@gmail.com>
2022-02-23 16:47:32 +0100

    💫 Add lottie confetti animation when solved

--- lib/presentation/widget/solved.dart
+++ lib/presentation/widget/solved.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class PuzzleSolved extends StatelessWidget {
   final String name;
@@ -7,8 +8,29 @@ class PuzzleSolved extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Puzzle solved! You unlocked $name"),
+    return Stack(
+      children: [
+        Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Puzzle solved! You found",
+                style: TextStyle(fontSize: 20),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                name,
+                style: const TextStyle(
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Center(child: Lottie.asset('assets/confetti.json')),
+      ],
     );
   }
 }
